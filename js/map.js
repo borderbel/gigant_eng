09d9b86b8da65d859f52fafdd22d7153668aba93
Sergey Tarliuk <starluk@mail.ru>
2019-02-20 09:32:09 +0300

    structure

---
+++ js/map.js
@@ -0,0 +1,21 @@
+'use strict';
+// создание экземпляра карты
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map("map", {
+            center: [54.83, 37.11],
+            zoom: 5
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark([55.907228, 31.260503], {
+            // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+            balloonContentHeader: "Балун метки",
+            balloonContentBody: "Содержимое <em>балуна</em> метки",
+            balloonContentFooter: "Подвал",
+            hintContent: "Хинт метки"
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+}
